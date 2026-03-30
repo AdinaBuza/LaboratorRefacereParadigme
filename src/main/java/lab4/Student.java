c806f2138c3fa85a178754a9a744d22be65e46c1
AdinaBuza <adinabuza15@gmail.com>
2026-03-30 19:27:20 +0300

    rezolvare 4.5.2

---
+++ src/main/java/lab4/Student.java
@@ -0,0 +1,45 @@
+package lab4;
+
+import java.util.Objects;
+
+public class Student {
+    Integer numarMatricol;
+    String prenume;
+    String nume;
+    String formatieDeStudiu;
+    float nota;
+
+    public Student(Integer numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(numarMatricol, student.numarMatricol);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numarMatricol);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "ID=" + numarMatricol +
+                ", nume='" + prenume + " " + nume + '\'' +
+                ", grupa='" + formatieDeStudiu + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+}
