bca031e8a88ead0f34cf41548c7742bd8f8efde2
AdinaBuza <adinabuza15@gmail.com>
2026-03-30 18:41:50 +0300

    laborator 1

---
+++ src/main/java/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student(12345, "Ion", "Popescu", "Informatica");
+
+
+        System.out.println(student1);
+    }
+}
