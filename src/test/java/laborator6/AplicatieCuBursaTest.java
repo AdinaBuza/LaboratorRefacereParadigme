551c2d588437f718e57c05c3b15ded237b91a42e
AdinaBuza <adinabuza15@gmail.com>
2026-04-22 08:54:29 +0300

    Lab6 Rezolvare corectare tema 6.8.3

--- src/test/java/laborator6/AplicatieCuBursaTest.java
+++ src/test/java/laborator6/AplicatieCuBursaTest.java
@@ -4,33 +4,40 @@ import lab5.StudentBursier;
 import lab6.AplicatieCuBursa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class AplicatieCuBursaTest {
     AplicatieCuBursa appCuBursa = new AplicatieCuBursa();
 
     @Test
+
+
     void sortTest1() {
-        List<StudentBursier> lista = appCuBursa.genereaza();
 
-        appCuBursa.sorteaza(lista);
+        List<StudentBursier> lista = new ArrayList<>();
+        StudentBursier bursier1 = new StudentBursier(1025, "Andrei", "Popa", "ISM141/2", 8.70f, 725.50);
+        lista.add(bursier1);
+        StudentBursier bursier2 = new StudentBursier(1024, "Ioan", "Mihalcea", "ISM141/1", 9.80f, 801.10);
+        lista.add(bursier2);
+        StudentBursier bursier3 = new StudentBursier(1029, "Bianca", "Popescu", "TI131/1", 9.10f, 780.80);
+        lista.add(bursier3);
+        StudentBursier bursier4 = new StudentBursier(1026, "Anamaria", "Prodan", "TI131/1", 8.90f, 745.50);
+        lista.add(bursier4);
+        StudentBursier bursier5 = new StudentBursier(1029, "Bianca", "Popescu", "TI131/1", 9.10f, 100.00);
+        lista.add(bursier5);
 
-        for (int i = 0; i < lista.size() - 1; i++) {
-            StudentBursier curent = lista.get(i);
-            StudentBursier urmator = lista.get(i + 1);
+
+        List<StudentBursier> lista_sortata= appCuBursa.sorteaza(lista);
+
+        Assertions.assertEquals(lista_sortata.get(0).getNume(),"Mihalcea");
+        Assertions.assertEquals(lista_sortata.get(1).getNume(),"Popa");
+        Assertions.assertEquals(lista_sortata.get(2).getNume(),"Popescu");
+        Assertions.assertEquals(lista_sortata.get(3),bursier3);
+        Assertions.assertEquals(lista_sortata.get(4),bursier4);
 
 
-            int compFormatie = curent.getFormatieDeStudiu().compareTo(urmator.getFormatieDeStudiu());
 
-            Assertions.assertTrue(compFormatie <= 0,
-                    "Eroare sortare la indexul " + i + ": Formatia nu este in ordine alfabetica!");
-
-
-            if (compFormatie == 0) {
-                int compNume = curent.getNume().compareTo(urmator.getNume());
-                Assertions.assertTrue(compNume <= 0,
-                        "Eroare sortare la indexul " + i + ": Numele nu este in ordine in aceeasi grupa!");
-            }
-        }
     }
 }
